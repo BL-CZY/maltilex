1eabdf4b656a95a79980664416aca975270d23b7
BL-CZY <jiguangllsw@gmail.com>
2025-01-05 13:11:56 +0100

    nav

--- app/src/routes/search/+page.server.ts
+++ app/src/routes/search/+page.server.ts
@@ -1,6 +1,12 @@
 export const actions = {
-	default: async () => {
-		console.log('hi');
-		return { a: 'hi' };
+	default: async ({ fetch }) => {
+		try {
+			let res = await fetch('http://127.0.0.1:3000/');
+			let data = await res.json();
+			return { a: 'a' };
+		} catch (e) {
+			// console.log(e);
+			return { err: true };
+		}
 	}
 };
