e383f82e7d95c519e3dd88609ffdcc75eb9bc189
BL-CZY <jiguangllsw@gmail.com>
2025-01-08 19:15:38 +0100

    some ui changes

--- app/src/lib/nav.svelte
+++ app/src/lib/nav.svelte
@@ -7,7 +7,7 @@
     };
 </script>
 
-<header class="bg-base-100 border-base-200 border-b shadow-sm">
+<header class="bg-base-100 border-base-200 fixed left-0 right-0 top-0 z-10 border-b shadow-sm">
     <div class="navbar mx-auto flex min-h-16 max-w-7xl items-center justify-center px-4">
         <div class="flex-none gap-2"><a class="btn btn-ghost btn-md" href="/">Home</a></div>
         <form class="flex-1 justify-center" on:submit|preventDefault={submit}>
@@ -24,3 +24,5 @@
         <div class="flex-none gap-2"><a class="btn btn-ghost btn-md" href="/about">About</a></div>
     </div>
 </header>
+
+<div class="h-[70px]"></div>
--- app/src/lib/search-entry.svelte
+++ app/src/lib/search-entry.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-	type Props = {
-		word: string;
-		pos: string;
-		matched: string;
-		en: Array<string>;
-	};
+    type Props = {
+        word: string;
+        pos: string;
+        matched: string;
+        en: Array<string>;
+    };
 
-	let props: Props = $props();
-	let { word, pos, matched, en } = props;
+    let props: Props = $props();
+    let { word, pos, matched, en } = props;
 </script>
 
-<div class="my-2 flex min-h-[100px] w-full justify-center">
-	<button
-		class="card bg-base-100 hover:bg-accent hover:text-accent-content w-[90%] p-4 shadow-md transition-all duration-300"
-	>
-		<div class="flex items-start justify-between gap-4">
-			<div class="ml-10 flex-1 text-left">
-				<p class="text-lg font-medium">{word}</p>
-				<p class="text-base-content/70 text-sm">matched: {matched}</p>
-			</div>
+<div class="my-2 flex w-full justify-center">
+    <button
+        class="btn bg-base-100 hover:bg-accent hover:text-accent-content min-h-[100px] w-[90%] p-4 shadow-md"
+    >
+        <div class="flex w-full items-start justify-between gap-4">
+            <div class="ml-10 flex-1 text-left">
+                <p class="text-lg font-medium">{word}</p>
+                <p class="text-base-content/70 text-sm">matched: {matched}</p>
+            </div>
 
-			<div class="ml-10 flex-1 text-left">
-				<p class="text-base">{pos}.</p>
-			</div>
+            <div class="ml-10 flex-1 text-left">
+                <p class="text-base">{pos}.</p>
+            </div>
 
-			<div class="flex flex-[8] flex-col items-start space-y-1">
-				{#each en as ele}
-					<p class="text-lef text-sm">{ele}</p>
-				{/each}
-			</div>
-		</div>
-	</button>
+            <div class="flex flex-[8] flex-col items-start space-y-1">
+                {#each en as ele}
+                    <p class="text-lef text-sm">{ele}</p>
+                {/each}
+            </div>
+        </div>
+    </button>
 </div>
