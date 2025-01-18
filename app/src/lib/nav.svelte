bd951a547691eca4e1f534a9018cc4202b02cd4b
BL-CZY <jiguangllsw@gmail.com>
2025-01-18 15:16:34 +0100

    stuff

--- app/src/lib/nav.svelte
+++ app/src/lib/nav.svelte
@@ -1,26 +1,11 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-
-    let value: string;
-    const submit = async () => {
-        await goto(`/search?keyword=${value.trim().toLocaleLowerCase()}`);
-    };
+    import SearchBar from './search-bar.svelte';
 </script>
 
 <header class="bg-base-100 border-base-200 fixed left-0 right-0 top-0 z-10 border-b shadow-sm">
     <div class="navbar mx-auto flex min-h-16 max-w-7xl items-center justify-center px-4">
         <div class="flex-none gap-2"><a class="btn btn-ghost btn-md" href="/">Home</a></div>
-        <form class="flex-1 justify-center" on:submit|preventDefault={submit}>
-            <input
-                class="input input-sm input-bordered w-[50%]"
-                placeholder="Search something..."
-                type="text"
-                name="keyword"
-                bind:value
-                required
-            />
-            <input class="btn btn-sm ml-2" type="submit" value="search" />
-        </form>
+        <SearchBar />
         <div class="flex-none gap-2"><a class="btn btn-ghost btn-md" href="/about">About</a></div>
     </div>
 </header>
---
+++ app/src/lib/search-bar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { slide } from 'svelte/transition';
+
+    let value: string = $state('');
+    let isPanelVisible = $state(false);
+
+    const submit = async () => {
+        await goto(`/search?keyword=${value.trim().toLocaleLowerCase()}`);
+    };
+</script>
+
+<form class="flex-1 justify-center" on:submit|preventDefault={submit}>
+    <div></div>
+    <input
+        class="input input-sm input-bordered w-[50%]"
+        placeholder="Search something..."
+        type="text"
+        name="keyword"
+        bind:value
+        required
+    />
+    <input class="btn btn-sm ml-2" type="submit" value="search" />
+</form>
