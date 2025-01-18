c7770c231f8f631d077c4b83ec7cdc637265bfd2
BL-CZY <jiguangllsw@gmail.com>
2025-01-18 11:21:44 +0100

    expand

--- api/src/word.rs
+++ api/src/word.rs
@@ -1,13 +1,110 @@
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use axum::{extract::Path, Json};
+use lazy_static::lazy_static;
 use mongodb::{
-    bson::{doc, oid::ObjectId, Document},
+    bson::{doc, oid::ObjectId, Bson, Document},
     Collection, Database,
 };
 use serde::Serialize;
 
 use crate::db::CLIENT;
+
+lazy_static! {
+    static ref WORD_KEYS_TABLE: HashMap<String, String> = {
+        [
+            ("w", "word"),
+            ("ph", "phonetic"),
+            ("p", "part_of_speech"),
+            ("r", "root"),
+            ("f", "forms"),
+            ("ed", "en_display"),
+            ("ex", "examples"),
+            ("c", "contributors"),
+        ]
+        .into_iter()
+        .map(|s| (s.0.to_string(), s.1.to_string()))
+        .collect()
+    };
+    static ref FORM_KEYS_TABLE: HashMap<String, String> = {
+        [
+            ("w", "word"),
+            ("ph", "phonetic"),
+            ("en", "english"),
+            ("n", "num"),
+            ("g", "gender"),
+            ("p", "part_of_speech"),
+            ("s", "subject"),
+            ("o", "object"),
+            ("on", "object_number"),
+            ("og", "object_gender"),
+            ("io", "indirect_object"),
+            ("ion", "indirect_object_number"),
+            ("iog", "indirect_object_gender"),
+            ("t", "tense"),
+        ]
+        .into_iter()
+        .map(|s| (s.0.to_string(), s.1.to_string()))
+        .collect()
+    };
+    static ref FORM_VAL_TABLE: HashMap<String, String> = {
+        [
+            ("sg", "singular"),
+            ("pl", "plural"),
+            ("m", "male"),
+            ("f", "female"),
+            ("p", "perfect"),
+            ("ip", "imperfect"),
+            ("im", "imperative"),
+            ("pa", "passive"),
+        ]
+        .into_iter()
+        .map(|s| (s.0.to_string(), s.1.to_string()))
+        .collect()
+    };
+}
+
+fn expand_word(mut doc: Document) -> Document {
+    if let Ok(arr) = doc.get_array_mut("f") {
+        arr.iter_mut().for_each(|d| {
+            if let Some(form) = d.as_document_mut() {
+                // set values
+                form.iter_mut().for_each(|(k, v)| {
+                    if k == "w" || k == "ph" || k == "en" {
+                        return;
+                    }
+
+                    if let Some(a) = v.as_array_mut() {
+                        a.iter_mut().for_each(|ele| {
+                            if let Bson::String(field) = ele {
+                                if let Some(val) = FORM_VAL_TABLE.get(field) {
+                                    *ele = Bson::String(val.clone());
+                                }
+                            }
+                        });
+                    }
+                });
+
+                // set keys
+                FORM_KEYS_TABLE.iter().for_each(|(key, replace)| {
+                    if let Some(b) = form.get(key) {
+                        form.insert(replace, b.clone());
+                        form.remove(key);
+                    }
+                });
+            }
+        });
+    }
+
+    WORD_KEYS_TABLE.iter().for_each(|(key, replace)| {
+        if let Some(b) = doc.get(key) {
+            doc.insert(replace, b.clone());
+            doc.remove(key);
+        }
+    });
+
+    doc
+}
 
 pub async fn get_word(Path(str): Path<String>) -> Json<WordResult> {
     let id = if let Ok(id) = ObjectId::from_str(&str) {
@@ -29,7 +126,7 @@ pub async fn get_word(Path(str): Path<String>) -> Json<WordResult> {
 
             Json(WordResult {
                 error: None,
-                word: Some(doc),
+                word: Some(expand_word(doc)),
             })
         }
         _ => Json(WordResult {
