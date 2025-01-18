cee92c11e44abd1a8e7b29c59455361a9d02ba84
BL-CZY <jiguangllsw@gmail.com>
2025-01-18 18:15:09 +0100

    quick about

---
+++ app/src/routes/about/+page.svelte
@@ -0,0 +1,34 @@
+<div class="card card-body bg-base-200 mx-auto h-screen max-w-7xl px-4 shadow-lg">
+    <div class="mt-10">
+        <h1 class="text-2xl font-bold">1 Navigation</h1>
+        <hr class="divider" />
+        <p>
+            You can put words in the search bar, and you can also click the "Show Advanced Options"
+            button to configure it
+        </p>
+    </div>
+
+    <div class="mt-10">
+        <h1 class="text-2xl font-bold">2 Contribution</h1>
+
+        <hr class="divider" />
+        <h2 class="text-xl font-bold">2.1 Source code</h2>
+        <hr class="divider" />
+        <p>
+            The source code is available at <a
+                class="link link-info"
+                href="https://github.com/BL-CZY/maltilex">here</a
+            >. If you want to make any change, feel free to post issues or create a pull request.
+        </p>
+
+        <hr class="divider" />
+        <h2 class="text-xl font-bold">2.2 Database</h2>
+        <hr class="divider" />
+        <p>
+            If you want to add words to the database, feel free to go to <a
+                class="link link-info"
+                href="/contributors">here</a
+            > to add words
+        </p>
+    </div>
+</div>
