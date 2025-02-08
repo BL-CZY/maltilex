4a1f7b1a2bcf1562a52a05e92e223c75db184701
BL-CZY <jiguangllsw@gmail.com>
2025-02-08 18:23:34 +0100

    o

--- dev/src/routes/dashboard/success/+page.ts
+++ dev/src/routes/dashboard/success/+page.ts
@@ -1,6 +1,6 @@
 export const load = ({ url }) => {
     return {
         url: url.searchParams.get('url') ?? '/dashboard',
-        msg: url.searchParams.get('msg') ?? 'Some problems happened'
+        msg: url.searchParams.get('msg') ?? 'Success!'
     };
 };
